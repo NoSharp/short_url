5019adc53a9fe8a9a7ecb940f81102399dc5f3f2
Harry <33969918+NoSharp@users.noreply.github.com>
2019-02-17 10:51:46 +0000

    Refactor

--- source/Shortner.d
+++ source/Shortner.d
@@ -1,7 +1,6 @@
 import  vibe.vibe;
 import Shorten;
 
-
 final class Shortner{
     public:
         Shorten[] shortens;
@@ -10,19 +9,30 @@ final class Shortner{
 
     void get() {
 		render!("index.dt");
-	}
+    }
 
-
-    
-    void postShortner(string url){
-        auto shorten = new Shorten(url, "rick");
+    @path("/shrink")
+    void postShortner(HTTPServerRequest req, HTTPServerResponse res, string url, string name){
+        auto shorten = new Shorten(url, name);
         this.shortens ~= shorten;
-        redirect(url);
+        logInfo("got called with url: %s", url);
+       
+        render!("empty.dt");
+        res.writeBody("<h1> " ~ name ~ " </h1>");
     }
 
-    void getURL(string id){
-
-
-    }
+    @path("/:id")
+    void index(string _id)
+	{
+		logInfo("got called with A id: %s", _id);
+		foreach (Shorten shorte ; shortens){
+			logInfo(shorte.url);
+			if(shorte.target == _id){
+				redirect(shorte.url);
+				return;
+			}
+		}
+		logInfo("Found none. %s", _id);
+	}
 
 }
